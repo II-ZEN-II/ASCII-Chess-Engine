d9031cbb82eb76441400ce6fb0619a19ce2e14d4
ZEN <97170564+II-ZEN-II@users.noreply.github.com>
2022-10-03 08:16:12 +1100

    v2
    - improved move parsing
    - added pawn promotion
    - added check
    - added stalemate
    - added checkmate
    - game-over state
    - only display valid moves
    - made a bit of a mess

--- ASCII Chess Engine/program.py
+++ ASCII Chess Engine/program.py
@@ -2,9 +2,10 @@ import board
 
 def main():
     game = board.Board()
-    while True: #TODO change to not GAMEOVER
+    while game.get_gameover() == False:
         game.display()
         game.player_turn()
+        
     
 if __name__ == '__main__':
     main()
